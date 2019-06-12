a27e200318e7e65b485d08723462985e94e5dafa
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-06-12 00:03:57 -0500

    Add background image

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # My webpage
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/fa65ad2c-0593-4a6b-a935-a3ac02446a20/deploy-status)](https://app.netlify.com/sites/thirsty-swirles-3e51cf/deploys)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
@@ -25,6 +25,6 @@ const IndexPage = () => (
       </li>
     </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
