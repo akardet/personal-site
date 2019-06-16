3fedd920602be9af971384fa69ff5c9746e54dc2
Sam Tancharoensuksavai <stancharoen@gmail.com>
2019-06-16 00:05:12 -0500

    Update landing page; Added style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,47 +4,40 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import GithubIcon from "../components/icons/github";
 import TwitterIcon from "../components/icons/twitter";
+import LinkedinIcon from "../components/icons/linkedin";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Sam Tancharoensuksavai</h1>
     <ul className="nav">
-      <li className="nav__link nav__link-first">
-        <a
-          href="https://github.com/akardet"
-          className="nav__item nav__item-first"
-        >
-          <GithubIcon />
+      <li className="nav__btn">
+        <a href="https://github.com/akardet" className="nav__item">
+          <GithubIcon /> <span className="nav__item-title">GitHub</span>
         </a>
       </li>
-      <li className="nav__link">
+      <li className="nav__btn">
         <a href="https://twitter.com/null_ux" className="nav__item">
           <TwitterIcon />
+          <span className="nav__item-title">Twitter</span>
         </a>
       </li>
-      <li className="nav__link">
-        <a href="#" className="nav__item nav__item-btn">
-          Resume
-        </a>
-      </li>
-      <li className="nav__link">
-        <a href="#" className="nav__item nav__item-btn">
-          Contact
+      <li className="nav__btn">
+        <a
+          href="https://www.linkedin.com/in/sam-tancharoensuksavai/"
+          className="nav__item"
+        >
+          <LinkedinIcon /> <span className="nav__item-title">LinkedIn</span>
         </a>
       </li>
     </ul>
-    <p>
-      Retro tempor waistcoat sunt, cornhole enim aesthetic. Aliqua hexagon
-      dreamcatcher food truck, incididunt cillum quis fingerstache you probably
-      haven't heard of them meditation lumbersexual mollit in cronut forage.
-      Adaptogen bushwick man bun, fashion axe cardigan schlitz roof party af
-      enamel pin direct trade cray paleo veniam voluptate. Craft beer gastropub
-      eiusmod marfa, deep v roof party incididunt cornhole chia humblebrag
-      godard excepteur semiotics sint letterpress. Pop-up kale chips irony quis
-      keffiyeh green juice nostrud dolor ex aute mlkshk in chicharrones deserunt
-      lomo. Aliqua quis sriracha mixtape pork belly.
-    </p>
+    <div className="container">
+      <div className="container__hero">
+        <h1>
+          Hi <br /> I'm Sam
+        </h1>
+        <h2>I like to design and build things for the web</h2>
+      </div>
+    </div>
   </Layout>
 );
 
